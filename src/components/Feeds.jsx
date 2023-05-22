7672b840383f45e4c643219c91902a03c1eb873f
isaiah usiholo <evansaizik@gmail.com>
2023-05-22 08:45:30 +0100

    added posts

--- src/components/Feeds.jsx
+++ src/components/Feeds.jsx
@@ -1,31 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Navbar from './Navbar';
 import axiosClient from '../utils/axiosInstance';
 
 const Feeds = () => {
-  // let mountRef = useRef(false);
+  let mountRef = useRef(false);
+  const [data, setData] = useState({});
 
   useEffect(() => {
-    // if (mountRef.current === true) {
+    if (mountRef.current === true) {
       const getFeeds = async () => {
         try {
           const response = await axiosClient.get('/posts');
-          console.log(response?.data);
+          setData(response?.data);
         } catch (error) {
           console.error(error);
         }
       };
       getFeeds();
-    // }
-    // return () => (mountRef.current = true);
+    }
+    return () => (mountRef.current = true);
   }, []);
 
   return (
     <section className="feeds">
       <Navbar />
       <div className="posts">
-        <p>this is my first post</p>
-        <p>Isaiah</p>
+        {data?.data?.posts.map((post, i) => {
+          return (
+            <div key={i}>
+              <p>{post.comment}</p>
+              <p>{post.author}</p>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
